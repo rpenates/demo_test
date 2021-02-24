d947bf66f326c433bd1d080a1289e05341868e52
Rafael Peñates <andres.penates@gmail.com>
2021-02-24 14:08:16 -0500

    Initial Commit

---
+++ CaptureExperiments/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CaptureExperiments
+//
+//  Created by Rafael Peñates on 24/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
